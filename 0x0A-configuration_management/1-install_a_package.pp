c9e8c567ab21cfb0db75f54fa7291d81f3294dac
MaryomaElsayed <maryamelsayd.1@gmail.com>
2024-01-07 19:34:59 -0800

    e

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Installs puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => gem,
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
